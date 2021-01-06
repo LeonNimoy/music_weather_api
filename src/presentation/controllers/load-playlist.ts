190d4cac7c3f736d59198e9a3dfbea37bbfeb6d9
Leonardo Cunha <leonardo.msc42@gmail.com>
2021-01-06 06:44:40 -0300

    refactor: make LoadPlaylistController conforms to a protocol

--- src/presentation/controllers/load-playlist.ts
+++ src/presentation/controllers/load-playlist.ts
@@ -1,7 +1,8 @@
+import { Controller } from '../protocols/controller'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-erro'
 import { badRequest } from '../helpers/http-helper'
-export class LoadPlayListController {
+export class LoadPlayListController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.param.city_name) {
       return badRequest(new MissingParamError('city_name'))
