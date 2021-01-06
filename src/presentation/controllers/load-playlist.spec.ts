26c6d5366f07b68001d758e65d9c3de0299f9eb5
Leonardo Cunha <leonardo.msc42@gmail.com>
2021-01-06 10:20:49 -0300

    test: ensure LoadPlaylistController calls WeatherProvider with correct city param

--- src/presentation/controllers/load-playlist.spec.ts
+++ src/presentation/controllers/load-playlist.spec.ts
@@ -1,13 +1,30 @@
 import { LoadPlayListController } from './load-playlist'
 import { MissingParamError } from '../errors/missing-param-erro'
+import { WeatherProvider } from '../protocols/weather-provider'
 
-const makeSut = (): LoadPlayListController => {
-  return new LoadPlayListController()
+interface SutTypes {
+  sut: LoadPlayListController
+  weatherProviderStub: WeatherProvider
+}
+
+const makeSut = (): SutTypes => {
+  class WeatherProviderStub implements WeatherProvider {
+    load (city: string): number {
+      return 1
+    }
+  }
+  const weatherProviderStub = new WeatherProviderStub()
+
+  const sut = new LoadPlayListController(weatherProviderStub)
+  return {
+    sut,
+    weatherProviderStub
+  }
 }
 
 describe('Load Playlist Controller', () => {
   test('should return 400 if no city name is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       param: {
       }
@@ -15,5 +32,17 @@ describe('Load Playlist Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('city_name'))
+  })
+
+  test('should call WeatherProvider with correct city param', () => {
+    const { sut, weatherProviderStub } = makeSut()
+    const loadSpy = jest.spyOn(weatherProviderStub, 'load')
+    const httpRequest = {
+      param: {
+        city_name: 'any_city'
+      }
+    }
+    sut.handle(httpRequest)
+    expect(loadSpy).toHaveBeenCalledWith('any_city')
   })
 })
