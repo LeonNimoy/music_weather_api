55d2c563f39ed9aea6eeeeae6ac050189b1e0307
Leonardo Cunha <leonardo.msc42@gmail.com>
2021-01-06 10:41:24 -0300

    refactor: move server error to a helper method

--- src/presentation/controllers/load-playlist.ts
+++ src/presentation/controllers/load-playlist.ts
@@ -1,9 +1,8 @@
 import { Controller } from '../protocols/controller'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-erro'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, serverError } from '../helpers/http-helper'
 import { WeatherProvider } from '../protocols/weather-provider'
-import { ServerError } from '../errors/server-error'
 
 export class LoadPlayListController implements Controller {
   private readonly weatherProvider: WeatherProvider
@@ -20,10 +19,7 @@ export class LoadPlayListController implements Controller {
 
       this.weatherProvider.load(httpRequest.param.city_name)
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: new ServerError()
-      }
+      return serverError()
     }
   }
 }
