05f5e259d156e82751d40c5423f7a0bbb143860f
Leonardo Cunha <leonardo.msc42@gmail.com>
2021-01-05 21:19:09 -0300

    feat: ensure LoadPlaylistController returns an error if no city name is provided

--- src/presentation/controllers/load-playlist.ts
+++ src/presentation/controllers/load-playlist.ts
@@ -1,7 +1,8 @@
 export class LoadPlayListController {
   handle (httpRequest: any): any {
     return {
-      statusCode: 400
+      statusCode: 400,
+      body: new Error('Missing param: city_name')
     }
   }
 }
