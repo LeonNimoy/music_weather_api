08c4eaf7829483178249d084e27a558cd26b09e6
Leonardo Cunha <leonardo.msc42@gmail.com>
2021-01-06 06:32:49 -0300

    refactor: move duplicated code to a helper method

--- src/presentation/controllers/load-playlist.ts
+++ src/presentation/controllers/load-playlist.ts
@@ -1,13 +1,10 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-erro'
-
+import { badRequest } from '../helpers/http-helper'
 export class LoadPlayListController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.param.city_name) {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('city_name')
-      }
+      return badRequest(new MissingParamError('city_name'))
     }
   }
 }
