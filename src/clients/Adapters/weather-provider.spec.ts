16c3ae8b16a95c69d745326e3d7d7ba023a03500
Leonardo Cunha <leonardo.msc42@gmail.com>
2021-01-07 20:54:43 -0300

    test: ensure WeatherProviderAdapter returns valid data, using city as param

---
+++ src/clients/Adapters/weather-provider.spec.ts
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+import { WeatherProviderAdapter } from './weather-provider'
+
+describe('Weather Provider Adapter', () => {
+  test('should return a temperature from Open Weather service, passing city name as param', async () => {
+    const city = 'London'
+
+    jest.spyOn(axios, 'get').mockResolvedValue({
+      data: {
+        main: {
+          temp: 12
+        }
+      }
+    })
+
+    const sut = new WeatherProviderAdapter(axios)
+    const temperature = await sut.loadUsingCity(city)
+    expect(temperature).toEqual(12)
+  })
+})
