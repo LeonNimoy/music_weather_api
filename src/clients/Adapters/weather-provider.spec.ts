be271a749e79fa189140341fadec5d67dd54aeea
Leonardo Cunha <leonardo.msc42@gmail.com>
2021-01-08 06:12:54 -0300

    test: ensure WeatherProviderAdapter returns valid data, using geographical coordinates as param

--- src/clients/Adapters/weather-provider.spec.ts
+++ src/clients/Adapters/weather-provider.spec.ts
@@ -18,4 +18,21 @@ describe('Weather Provider Adapter', () => {
     const temperature = await sut.loadUsingCity(city)
     expect(temperature).toEqual(12)
   })
+
+  test('should return a temperature from Open Weather service, passing geographical coordinates as param', async () => {
+    const lat = '123'
+    const long = '456'
+
+    jest.spyOn(axios, 'get').mockResolvedValue({
+      data: {
+        main: {
+          temp: 12
+        }
+      }
+    })
+
+    const sut = new WeatherProviderAdapter(axios)
+    const temperature = await sut.loadUsingGeographicalCoordinates(lat, long)
+    expect(temperature).toEqual(12)
+  })
 })
