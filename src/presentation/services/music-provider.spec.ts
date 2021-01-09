97dc6c11d0dbde3bde995aff9fd689d324972e45
Leonardo Cunha <leonardo.msc42@gmail.com>
2021-01-09 12:42:08 -0300

    test: ensure that MusicProviderService returns a rock playlist

--- src/presentation/services/music-provider.spec.ts
+++ src/presentation/services/music-provider.spec.ts
@@ -28,7 +28,7 @@ const makeSut = (): SutTypes => {
 }
 
 describe('Music Provider Service', () => {
-  test('should call Music Provider using Party genre, if the temperature is above 30° Celsius', async () => {
+  test('should call Music Provider passing Party genre as param, if the temperature is above 30° Celsius', async () => {
     const { musicProviderAdapterStub, sut } = makeSut()
     const temperature = 31
 
@@ -36,5 +36,25 @@ describe('Music Provider Service', () => {
 
     await sut.loadPlaylist(temperature)
     expect(loadSpy).toHaveBeenCalledWith('party')
+  })
+
+  test('should call Music Provider passing Pop genre as param, if the temperature is between 15° and 30° Celsius', async () => {
+    const { musicProviderAdapterStub, sut } = makeSut()
+    const temperature = 16
+
+    const loadSpy = jest.spyOn(musicProviderAdapterStub, 'load')
+
+    await sut.loadPlaylist(temperature)
+    expect(loadSpy).toHaveBeenCalledWith('pop')
+  })
+
+  test('should call Music Provider passing Rock genre as param, if the temperature is between 10° and 14° Celsius', async () => {
+    const { musicProviderAdapterStub, sut } = makeSut()
+    const temperature = 11
+
+    const loadSpy = jest.spyOn(musicProviderAdapterStub, 'load')
+
+    await sut.loadPlaylist(temperature)
+    expect(loadSpy).toHaveBeenCalledWith('rock')
   })
 })
