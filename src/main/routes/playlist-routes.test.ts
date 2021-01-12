c3c5ae96107df2c3c700701ab0878effe86828c6
Leonardo Cunha <leonardo.msc42@gmail.com>
2021-01-12 10:47:47 -0300

    test: create load playlist adapter in the main layer

--- src/main/routes/playlist-routes.test.ts
+++ src/main/routes/playlist-routes.test.ts
@@ -3,9 +3,23 @@ import request from 'supertest'
 import app from '../config/app'
 
 describe('Playlist Routes', () => {
-  test('should return a playlist on success', async () => {
+  test('should return a playlist on success, if city param is provided', async () => {
     await request(app)
       .get('/playlist')
+      .query({ city: 'Brasil' })
       .expect(200)
+  })
+
+  test('should return a playlist on success, if city param is provided', async () => {
+    await request(app)
+      .get('/playlist')
+      .query({ lat: '123', long: '456' })
+      .expect(200)
+  })
+
+  test.skip('should return 400, if no param is provided', async () => {
+    await request(app)
+      .get('/playlist')
+      .expect(400)
   })
 })
