163a88bfd060a39b4ad35c9023c01d4c5fe768dd
Leonardo Cunha <leonardo.msc42@gmail.com>
2021-01-12 10:44:26 -0300

    test: ensure laod playlist controller handle the data with two methods

--- src/presentation/controllers/load-playlist/load-playlist.spec.ts
+++ src/presentation/controllers/load-playlist/load-playlist.spec.ts
@@ -1,11 +1,12 @@
 import { LoadPlayListController } from './load-playlist'
 import { MissingQueryError, ServerError } from '../../errors'
 import { WeatherProvider } from './load-playlist-protocols'
-// import { MusicProviderService } from '../../services/music-provider'
-
+import { LoadPlaylist } from '../../../domain/usecases/load-playlist'
+import { Playlist } from '../../../domain/models/playlist'
 interface SutTypes {
   sut: LoadPlayListController
   weatherProviderStub: WeatherProvider
+  musicProviderServiceStub: LoadPlaylist
 }
 
 const makeWeatherProvider = (): WeatherProvider => {
@@ -14,92 +15,88 @@ const makeWeatherProvider = (): WeatherProvider => {
       return 1
     }
 
-    async loadUsingGeographicalCoordinates (lat: string): Promise<number> {
+    async loadUsingGeographicalCoordinates (lat: string, long: string): Promise<number> {
       return 1
     }
   }
   return new WeatherProviderStub()
 }
 
+const makeMusicProviderService = (): LoadPlaylist => {
+  class MusicProviderServiceStub implements LoadPlaylist {
+    async loadPlaylist (temperature: number): Promise<Playlist> {
+      return ['any_song1', 'any_song2']
+    }
+  }
+  return new MusicProviderServiceStub()
+}
+
 const makeSut = (): SutTypes => {
   const weatherProviderStub = makeWeatherProvider()
-  const sut = new LoadPlayListController(weatherProviderStub)
+  const musicProviderServiceStub = makeMusicProviderService()
+  const sut = new LoadPlayListController(weatherProviderStub, makeMusicProviderService())
   return {
     sut,
-    weatherProviderStub
+    weatherProviderStub,
+    musicProviderServiceStub
   }
 }
 
 describe('Load Playlist Controller', () => {
-  test.skip('should return 200 if a valid city data is provided', async () => {
+  test('should return 200 if a valid city data is provided', async () => {
     const { sut } = makeSut()
-    const httpRequest = {
-      query: {
-        city: 'any_city'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
+    const httpRequestCity = 'any_city'
+
+    const httpResponse = await sut.handleCity(httpRequestCity)
     expect(httpResponse.statusCode).toBe(200)
-    expect(httpResponse.body).toEqual(['any_music'])
+    expect(httpResponse.body).toEqual(['any_song1', 'any_song2'])
   })
 
-  test.skip('should return 200 if a valid geographic coordinates are provided', async () => {
+  test('should return 200 if some valid geographic coordinates are provided', async () => {
     const { sut } = makeSut()
-    const httpRequest = {
-      query: {
-        lat: '123',
-        long: '456'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
+    const httpRequestLat = '123'
+    const httpRequestLong = '456'
+
+    const httpResponse = await sut.handleGeographicalCoordinates(httpRequestLat, httpRequestLong)
     expect(httpResponse.statusCode).toBe(200)
-    expect(httpResponse.body).toEqual(['any_music'])
+    expect(httpResponse.body).toEqual(['any_song1', 'any_song2'])
   })
 
   test('should return 400 if no city name is provided', async () => {
     const { sut } = makeSut()
-    const httpRequest = {
-      query: {
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
+    const httpRequest = ''
+
+    const httpResponse = await sut.handleCity(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingQueryError())
   })
 
   test('should return 400 if no geographical coordinates are provided', async () => {
     const { sut } = makeSut()
-    const httpRequest = {
-      query: {
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
+    const httpRequestLat = ''
+    const httpRequestLong = ''
+    const httpResponse = await sut.handleGeographicalCoordinates(httpRequestLat, httpRequestLong)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingQueryError())
-  })
+  }
+  )
 
   test('should call WeatherProvider with correct city query', async () => {
     const { sut, weatherProviderStub } = makeSut()
     const loadSpy = jest.spyOn(weatherProviderStub, 'loadUsingCity')
-    const httpRequest = {
-      query: {
-        city: 'any_city'
-      }
-    }
-    await sut.handle(httpRequest)
+    const httpRequestCity = 'any_city'
+
+    await sut.handleCity(httpRequestCity)
     expect(loadSpy).toHaveBeenCalledWith('any_city')
   })
 
   test('should call WeatherProvider with the correct geographical coordinates query', async () => {
     const { sut, weatherProviderStub } = makeSut()
     const loadSpy = jest.spyOn(weatherProviderStub, 'loadUsingGeographicalCoordinates')
-    const httpRequest = {
-      query: {
-        lat: '123',
-        long: '456'
-      }
-    }
-    await sut.handle(httpRequest)
+    const httpRequestLat = '123'
+    const httpRequestLong = '456'
+
+    await sut.handleGeographicalCoordinates(httpRequestLat, httpRequestLong)
     expect(loadSpy).toHaveBeenCalledWith('123', '456')
   })
 
@@ -109,12 +106,9 @@ describe('Load Playlist Controller', () => {
       return await new Promise((resolve, reject) => reject(new Error()))
     })
 
-    const httpRequest = {
-      query: {
-        city: 'any_city'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
+    const httpRequestCity = 'any_city'
+
+    const httpResponse = await sut.handleCity(httpRequestCity)
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   })
@@ -125,45 +119,41 @@ describe('Load Playlist Controller', () => {
       return await new Promise((resolve, reject) => reject(new Error()))
     })
 
-    const httpRequest = {
-      query: {
-        lat: '123',
-        long: '456'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
+    const httpRequestLat = '123'
+    const httpRequestLong = '456'
+
+    const httpResponse = await sut.handleGeographicalCoordinates(httpRequestLat, httpRequestLong)
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   })
 
-  test.skip('should call Music Provider Service with correct temperature query', async () => {
-    const { sut, weatherProviderStub } = makeSut()
-    // const loadSpy = jest.spyOn(musicProviderServiceStub, 'loadPlaylist')
-    const httpRequest = {
-      query: {
-        city: 'any_city'
-      }
-    }
+  test('should call Music Provider Service with correct temperature query', async () => {
+    const { sut, musicProviderServiceStub, weatherProviderStub } = makeSut()
+    const loadSpy = jest.spyOn(musicProviderServiceStub, 'loadPlaylist')
+    const httpRequestCity = 'any_city'
 
-    await sut.handle(httpRequest)
+    await sut.handleCity(httpRequestCity)
 
-    const cityTemperature = await weatherProviderStub.loadUsingCity(httpRequest.query.city)
+    const cityTemperature = await weatherProviderStub.loadUsingCity(httpRequestCity)
 
-    expect(1).toHaveBeenCalledWith(cityTemperature)
+    await musicProviderServiceStub.loadPlaylist(cityTemperature)
+
+    expect(loadSpy).toHaveBeenCalledWith(cityTemperature)
   })
 
   test.skip('should return 500 if MusicProvider throws', async () => {
-    const { sut } = makeSut()
-    // jest.spyOn(musicProviderServiceStub, 'loadPlaylist').mockImplementationOnce(async () => {
-    //   return await new Promise((resolve, reject) => reject(new Error()))
-    // })
+    const { sut, musicProviderServiceStub, weatherProviderStub } = makeSut()
+    jest.spyOn(musicProviderServiceStub, 'loadPlaylist').mockImplementationOnce(async () => {
+      return await new Promise((resolve, reject) => reject(new Error()))
+    })
 
-    const httpRequest = {
-      query: {
-        city: 'any_city'
-      }
-    }
-    const httpResponse = await sut.handle(httpRequest)
+    const httpRequestCity = 'any_city'
+
+    const httpResponse = await sut.handleCity(httpRequestCity)
+    const cityTemperature = await weatherProviderStub.loadUsingCity(httpRequestCity)
+
+    await musicProviderServiceStub.loadPlaylist(cityTemperature)
+
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
   })
