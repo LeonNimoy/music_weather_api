dd76cb6806efd11dc8e1a8f514559bbf1c10f3af
Leonardo Cunha <leonardo.msc42@gmail.com>
2021-01-13 12:11:12 -0300

    test: ensure that MusicProviderAdapter is return a valid playlist form spotify service

--- src/clients/Adapters/music-provider.spec.ts
+++ src/clients/Adapters/music-provider.spec.ts
@@ -1,13 +1,12 @@
-import axios from 'axios'
 
 import { MusicProviderAdapter } from './music-provider'
 
 describe('Music Provider Adapter', () => {
-  test('should return a playlist from Spotify service', async () => {
-    const genre = 'any_genre'
-    const sut = new MusicProviderAdapter(axios)
+  test('should return a valid playlist from Spotify service', async () => {
+    const genre = 'rock'
+    const sut = new MusicProviderAdapter()
 
     const playlist = await sut.load(genre)
-    expect(playlist).toEqual(['any_song', 'any_song2'])
+    expect(playlist).toHaveProperty('items')
   })
 })
