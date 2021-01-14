b5c48046c1529f74b19cb86e6f89e43e136bc015
Leonardo Cunha <leonardo.msc42@gmail.com>
2021-01-13 22:15:39 -0300

    test: ensure tha LoadPlaylistController throws

--- src/presentation/controllers/load-playlist/load-playlist.spec.ts
+++ src/presentation/controllers/load-playlist/load-playlist.spec.ts
@@ -1,5 +1,4 @@
 import { LoadPlayListController } from './load-playlist'
-import { MissingQueryError, ServerError } from '../../errors'
 import { WeatherProvider } from './load-playlist-protocols'
 import { LoadPlaylist } from '../../../domain/usecases/load-playlist'
 import { Playlist } from '../../../domain/models/playlist'
@@ -62,25 +61,6 @@ describe('Load Playlist Controller', () => {
     expect(httpResponse.body).toEqual(['any_song1', 'any_song2'])
   })
 
-  test('should return 400 if no city name is provided', async () => {
-    const { sut } = makeSut()
-    const httpRequest = ''
-
-    const httpResponse = await sut.handleCity(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingQueryError())
-  })
-
-  test('should return 400 if no geographical coordinates are provided', async () => {
-    const { sut } = makeSut()
-    const httpRequestLat = ''
-    const httpRequestLong = ''
-    const httpResponse = await sut.handleGeographicalCoordinates(httpRequestLat, httpRequestLong)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new MissingQueryError())
-  }
-  )
-
   test('should call WeatherProvider with correct city query', async () => {
     const { sut, weatherProviderStub } = makeSut()
     const loadSpy = jest.spyOn(weatherProviderStub, 'loadUsingCity')
@@ -100,33 +80,6 @@ describe('Load Playlist Controller', () => {
     expect(loadSpy).toHaveBeenCalledWith('123', '456')
   })
 
-  test('should return 500 if WeatherProvider, receives a city but throws', async () => {
-    const { sut, weatherProviderStub } = makeSut()
-    jest.spyOn(weatherProviderStub, 'loadUsingCity').mockImplementationOnce(async () => {
-      return await new Promise((resolve, reject) => reject(new Error()))
-    })
-
-    const httpRequestCity = 'any_city'
-
-    const httpResponse = await sut.handleCity(httpRequestCity)
-    expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
-  })
-
-  test('should return 500 if WeatherProvider, receives geographical coordinates but throws', async () => {
-    const { sut, weatherProviderStub } = makeSut()
-    jest.spyOn(weatherProviderStub, 'loadUsingGeographicalCoordinates').mockImplementationOnce(async () => {
-      return await new Promise((resolve, reject) => reject(new Error()))
-    })
-
-    const httpRequestLat = '123'
-    const httpRequestLong = '456'
-
-    const httpResponse = await sut.handleGeographicalCoordinates(httpRequestLat, httpRequestLong)
-    expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
-  })
-
   test('should call Music Provider Service with correct temperature query', async () => {
     const { sut, musicProviderServiceStub, weatherProviderStub } = makeSut()
     const loadSpy = jest.spyOn(musicProviderServiceStub, 'loadPlaylist')
@@ -139,22 +92,5 @@ describe('Load Playlist Controller', () => {
     await musicProviderServiceStub.loadPlaylist(cityTemperature)
 
     expect(loadSpy).toHaveBeenCalledWith(cityTemperature)
-  })
-
-  test.skip('should return 500 if MusicProvider throws', async () => {
-    const { sut, musicProviderServiceStub, weatherProviderStub } = makeSut()
-    jest.spyOn(musicProviderServiceStub, 'loadPlaylist').mockImplementationOnce(async () => {
-      return await new Promise((resolve, reject) => reject(new Error()))
-    })
-
-    const httpRequestCity = 'any_city'
-
-    const httpResponse = await sut.handleCity(httpRequestCity)
-    const cityTemperature = await weatherProviderStub.loadUsingCity(httpRequestCity)
-
-    await musicProviderServiceStub.loadPlaylist(cityTemperature)
-
-    expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
   })
 })
