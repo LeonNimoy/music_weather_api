8a6a3759accabef6e7fd7647460e78b353cad02c
Leonardo Cunha <leonardo.msc42@gmail.com>
2021-01-14 10:48:09 -0300

    refactor: [wip]ensure that integration tests don't use .env

--- src/main/routes/playlist-routes.test.ts
+++ src/main/routes/playlist-routes.test.ts
@@ -2,24 +2,8 @@ import request from 'supertest'
 
 import app from '../config/app'
 
-describe('Playlist Routes', () => {
-  test('should return a playlist on success, if city param is provided', async () => {
-    await request(app)
-      .get('/playlist')
-      .query({ city: 'Brazil' })
-      .expect(200)
-  })
-
-  test('should return a playlist on success, if city param is provided', async () => {
-    await request(app)
-      .get('/playlist')
-      .query({ lat: '35', long: '139' })
-      .expect(200)
-  })
-
-  test('should return 400, if no param is provided', async () => {
-    await request(app)
-      .get('/playlist')
-      .expect(400)
-  })
+test('should return 400, if no param is provided', async () => {
+  await request(app)
+    .get('/playlist')
+    .expect(400)
 })
