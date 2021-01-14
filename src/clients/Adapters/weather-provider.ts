73a44a1c9981aa5c032c701824ef8e5087aa126e
Leonardo Cunha <leonardo.msc42@gmail.com>
2021-01-13 22:11:03 -0300

    feat: ensure tha WeatherProviderAdapter throws

--- src/clients/Adapters/weather-provider.ts
+++ src/clients/Adapters/weather-provider.ts
@@ -3,30 +3,63 @@ import fetch from 'node-fetch'
 
 import env from '../../main/config/env'
 import { WeatherProvider } from '../../presentation/protocols/weather-provider'
+import { NotFoundError, InvalidParamError } from '../../presentation/errors'
+import { notFound, badRequest } from '../../presentation/helpers/http-helper'
 
 export class WeatherProviderAdapter implements WeatherProvider {
-  public async loadUsingCity (city: string): Promise<number> {
-    const getCityTemperature = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=pt_br&appid=${env.openWeatherKey}`, {
-      method: 'GET'
-    })
+  public async loadUsingCity (city: string): Promise<any> {
+    try {
+      const getCityTemperature = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=pt_br&appid=${env.openWeatherKey}`, {
+        method: 'GET'
+      })
 
-    const getCityTemperatureResponse = await getCityTemperature.json()
+      if (getCityTemperature.status === 404) {
+        const getCityTemperatureResponse = await getCityTemperature.json()
+        console.error(getCityTemperatureResponse)
 
-    const temperature = getCityTemperatureResponse.main.temp
+        return notFound(new NotFoundError())
+      } else if (getCityTemperature.status === 400) {
+        const getCityTemperatureResponse = await getCityTemperature.json()
+        console.error(getCityTemperatureResponse)
 
-    return temperature
+        return badRequest(new InvalidParamError())
+      } else {
+        const getCityTemperatureResponse = await getCityTemperature.json()
+
+        const temperature = getCityTemperatureResponse.main.temp
+
+        return temperature
+      }
+    } catch (error) {
+      console.error(error)
+    }
   }
 
-  public async loadUsingGeographicalCoordinates (lat: string, long: string): Promise<number> {
-    const getGeographicalCoordinateTemperature = await fetch(`http://api.openweathermap.org/data/2.5/weather??lat=${lat}&lon=${long}&units=metric&lang=pt_br&appid=${env.openWeatherKey}`, {
-      method: 'GET'
-    })
-    const getGeographicalCoordinateTemperatureResponse = await getGeographicalCoordinateTemperature.json()
+  public async loadUsingGeographicalCoordinates (lat: string, long: string): Promise<any> {
+    try {
+      const getGeographicalCoordinateTemperature = await fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&lang=pt_br&appid=${env.openWeatherKey}`, {
+        method: 'GET'
+      })
 
-    console.log(getGeographicalCoordinateTemperatureResponse)
+      if (getGeographicalCoordinateTemperature.status === 400) {
+        const getGeographicalCoordinateTemperatureResponse = await getGeographicalCoordinateTemperature.json()
 
-    // const temperature = getGeographicalCoordinateTemperatureResponse.main.temp
+        console.error(getGeographicalCoordinateTemperatureResponse)
+        return badRequest(new InvalidParamError())
+      } else if (getGeographicalCoordinateTemperature.status === 404) {
+        const getGeographicalCoordinateTemperatureResponse = await getGeographicalCoordinateTemperature.json()
 
-    return 1
+        console.error(getGeographicalCoordinateTemperatureResponse)
+        return notFound(new NotFoundError())
+      } else {
+        const getGeographicalCoordinateTemperatureResponse = await getGeographicalCoordinateTemperature.json()
+
+        const temperature = getGeographicalCoordinateTemperatureResponse.main.temp
+
+        return temperature
+      }
+    } catch (error) {
+      console.error(error)
+    }
   }
 }
--- src/presentation/protocols/weather-provider.ts
+++ src/presentation/protocols/weather-provider.ts
@@ -1,4 +1,4 @@
 export interface WeatherProvider {
-  loadUsingCity: (city: string) => Promise<number>
-  loadUsingGeographicalCoordinates: (lat: string, long: string) => Promise<number>
+  loadUsingCity: (city: string) => Promise<any>
+  loadUsingGeographicalCoordinates: (lat: string, long: string) => Promise<any>
 }
