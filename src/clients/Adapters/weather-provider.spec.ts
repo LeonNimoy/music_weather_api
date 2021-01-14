a5ea15e5a943e54703bca002e088eca70a5e06a9
Leonardo Cunha <leonardo.msc42@gmail.com>
2021-01-13 22:11:21 -0300

    test: ensure tha WeatherProviderAdapter throws

--- src/clients/Adapters/weather-provider.spec.ts
+++ src/clients/Adapters/weather-provider.spec.ts
@@ -1,5 +1,6 @@
 
 import { WeatherProviderAdapter } from './weather-provider'
+import { InvalidParamError } from '../../presentation/errors'
 
 describe('Weather Provider Adapter', () => {
   test('should return a success promise from Open Weather service, passing a city name as param', async () => {
@@ -25,5 +26,16 @@ describe('Weather Provider Adapter', () => {
     await sut.loadUsingGeographicalCoordinates(lat, long)
 
     expect(loadSpy).toBeTruthy()
+  })
+
+  test('should return 400, if an invalid geographical coordinates is provided', async () => {
+    const lat = 'invalid_lat'
+    const long = 'invalid_long'
+
+    const sut = new WeatherProviderAdapter()
+
+    const musicProvider = await sut.loadUsingGeographicalCoordinates(lat, long)
+
+    expect(musicProvider.body).toEqual(new InvalidParamError())
   })
 })
